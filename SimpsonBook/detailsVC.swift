8c845e24f1c8a03a2f502e478a0e50eab01ab068
omerduman <dumanomer200@gmail.com>
2020-01-10 00:10:09 +0300

    TableView tanımlama kodları

---
+++ SimpsonBook/detailsVC.swift
@@ -0,0 +1,24 @@
+//
+//  detailsVC.swift
+//  SimpsonBook
+//
+//  Created by admin on 9.01.2020.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class detailsVC: UIViewController {
+
+    @IBOutlet weak var jobLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
