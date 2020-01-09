7570e7d09df44b38b417b4ba15b9a8ac2fec1853
omerduman <dumanomer200@gmail.com>
2020-01-10 00:34:12 +0300

    Simpson Classı yaratıp karakterlerin özelliklerini verip Array'e atadım.

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,24 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by admin on 10.01.2020.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Simpson{
+     var name : String
+      var job : String
+      var image : UIImage
+      
+      init(simpsonName: String, simpsonJob:String,simpsonImage:UIImage) {
+          
+          name = simpsonName
+          job = simpsonJob
+          image = simpsonImage
+          
+      }
+}
--- SimpsonBook/ViewController.swift
+++ SimpsonBook/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource{
 
+    var mySimpsons=[Simpson]()
+    
+    
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,16 +20,30 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
         tableView.dataSource=self
         tableView.delegate=self
         
-
+        let homer=Simpson(simpsonName: "Homer Simpson", simpsonJob: "Nuclear Safety", simpsonImage: UIImage(named: "homer")!)
+         
+        let marge = Simpson(simpsonName: "Marge Simpson", simpsonJob: "Housewife", simpsonImage: UIImage(named: "marge")!)
+             
+              let bart = Simpson(simpsonName: "Bart Simpson", simpsonJob: "Student", simpsonImage: UIImage(named: "bart")!)
+             
+              let lisa = Simpson(simpsonName: "Lisa Simpson", simpsonJob: "Student", simpsonImage: UIImage(named: "lisa")!)
+             
+              let maggie = Simpson(simpsonName: "Maggie Simpson", simpsonJob: "Baby", simpsonImage: UIImage(named: "maggie")!)
+        
+        mySimpsons.append(homer)
+        mySimpsons.append(marge)
+        mySimpsons.append(bart)
+        mySimpsons.append(lisa)
+        mySimpsons.append(maggie)
     }
    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return mySimpsons.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell=UITableViewCell()
-        cell.textLabel?.text="Homer Simpson"
+        cell.textLabel?.text=mySimpsons[indexPath.row].name
         return cell
     }
     
